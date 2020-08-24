1c5c21c095989795fe4803c9dcd253a0204f12f2
janakos <53950904+janakos@users.noreply.github.com>
2020-08-24 13:05:13 -0700

    rewriting transformation method - in progress

--- Swiftly.cs
+++ Swiftly.cs
@@ -9,16 +9,8 @@ class Swiflty
         Parser parser = new Parser();
         Dictionary<string, List<string>> parsedData = parser.ParseFile("C:\\Users\\alexj\\source\\repos\\swiftly\\sample.txt");
 
-        //foreach (KeyValuePair<string, List<string>> item in parsedData) {
-        //    Console.WriteLine(item.Key);
-        //    foreach (string str in item.Value)
-        //    {
-        //        Console.WriteLine(str);
-        //    }
-        //}
-
         Transformer transformer = new Transformer();
-        List<ProductRecord> finalData = transformer.TransToProductRecord(parsedData);
+        List<ProductRecord> finalData = transformer.TransformToProductRecord(parsedData);
 
         foreach (var r in finalData)
         {
--- Transformer.cs
+++ Transformer.cs
@@ -11,9 +11,10 @@ namespace DataProcessor
         // Used to store finalized data before turning into List<ProductRecord>
         Dictionary<string, List<object>> FinalizedData = new Dictionary<string, List<object>>();
 
-        public List<ProductRecord> TransToProductRecord(Dictionary<string, List<string>> initialData)
+        public List<ProductRecord> TransformToProductRecord(Dictionary<string, List<string>> initialData)
         {
-            SetFinalOutput(initialData);
+            //SetFinalOutput(initialData);
+            TransformAllColumns(initialData);
             List<ProductRecord> records = new List<ProductRecord>();
 
             for (int i = 0; i < this.FinalizedData["productId"].Count; i++)
@@ -49,6 +50,58 @@ namespace DataProcessor
         //    });
 
         //}
+
+        // change back to private
+        public void TransformAllColumns(Dictionary<string, List<string>> initialData)
+        {
+            InitializeFinalizedData();
+            Console.WriteLine(initialData["productId"].Count);
+            for (int i = 0; i < initialData["productId"].Count; i++)
+            {
+                Dictionary<string, string> currentRow = new Dictionary<string, string>();
+                Dictionary<string, object> finalizedRow = new Dictionary<string, object>();
+                foreach (string key in initialData.Keys)
+                {
+                    currentRow[key] = initialData[key][i];
+                }
+
+                // Do work here
+
+                finalizedRow["productId"] = currentRow["productId"];
+                finalizedRow["productDescription"] = currentRow["productDescription"];
+                finalizedRow["regularDisplayPrice"] = "0";
+                finalizedRow["regularCalculatorPrice"] = "0";
+                finalizedRow["promotionalDisplayPrice"] = "0";
+                finalizedRow["promotionalCalculatorPrice"] = "0";
+                finalizedRow["unitOfMeasure"] = "unit";
+                finalizedRow["productSize"] = "size";
+                finalizedRow["taxRate"] = 0;
+
+
+                // Write transformed data into FinalizedData: Dictionary<string, List<object>>
+                foreach (PropertyInfo property in typeof(ProductRecord).GetProperties())
+                {
+                    this.FinalizedData[property.Name].Add(finalizedRow[property.Name]);
+                }
+            }
+            //foreach (KeyValuePair<string, List<object>> item in this.FinalizedData)
+            //{
+            //    Console.WriteLine(item.Key);
+            //    foreach (object obj in item.Value)
+            //    {
+            //        Console.WriteLine(obj);
+            //    }
+            }
+        }
+
+        // Initialize keys of FinalizedData: Dictionary<string, List<object>>
+        private void InitializeFinalizedData()
+        {
+            foreach (PropertyInfo property in typeof(ProductRecord).GetProperties())
+            {
+                this.FinalizedData[property.Name] = new List<object>();
+            }
+        }
 
         private List<object> TransUnitOfMeasurement(List<string> data)
         {
